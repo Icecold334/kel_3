381eb4621bdd5458cf10d1ae551902117ce9517c
Icecold334 <fauzanimam334@gmail.com>
2024-05-03 07:40:41 +0700

    add products model

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/coba', [HomeController::class, 'coba']);
