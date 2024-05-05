e87275c75f4b64cab99d06215bb4ee071689f922
aldiapriliano <paudalghozali527@gmail.com>
2024-05-05 14:46:16 +0700

    push about

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-gradient bg-primary">
     <div class="container">
-        <a class="navbar-brand" href="index.html"><img class="rounded-circle me-1" src="assets/img/logo.jpg" alt="Bootstrap"
+        <a class="navbar-brand" href="/"><img class="rounded-circle me-1" src="assets/img/logo.jpg" alt="Bootstrap"
                 width="60" height="60" />
             <span class="nav-hover h5">Kobessah Kopi</span>
         </a>
@@ -14,7 +14,7 @@
                     <a class="nav-link nav-hover" href="menu.html">Menu</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-hover" href="about.html">Tentang Kami</a>
+                    <a class="nav-link nav-hover" href="/about">Tentang Kami</a>
                 </li>
             </ul>
             <div class="d-flex d-none">
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AboutController;
 
 Route::controller(HomeController::class)->group(function () {
   Route::get('/', 'index');
@@ -11,3 +12,4 @@ Route::controller(HomeController::class)->group(function () {
 
 Route::get('/manage', [DashboardController::class, 'index']);
 Route::resource('/products', ProductsController::class);
+Route::get('/about', [AboutController::class, 'index']);
